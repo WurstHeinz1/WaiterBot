f9375854f1b53565404f30db015237804a15ed72
Ch.Sch <75271618+WurstHeinz1@users.noreply.github.com>
2023-02-03 20:03:57 +0100

    added some error catching

--- languageProcessing.py
+++ languageProcessing.py
@@ -1,18 +1,24 @@
 import Levenshtein
 
 def checkIntent(input, keywords, margin):
-    for word in input.lower().split(" "):
-        for keyword in keywords:
-            if Levenshtein.distance(word, keyword.lower()) <= margin:
-                return True
-                break
+    try:
+        for word in input.lower().split(" "):
+            for keyword in keywords:
+                if Levenshtein.distance(word, keyword.lower()) <= margin:
+                    return True
+                    break
+        return False
+    except:
+        return False
 
 
 def analyzeIntent(input, keywords, margin):
     recognizedWords = []
-
-    for word in input.lower().split(" "):
-        for keyword in keywords:
-            if Levenshtein.distance(word, keyword.lower()) <= margin:
-                recognizedWords.append(word)
-    return recognizedWords
+    try:
+        for word in input.lower().split(" "):
+            for keyword in keywords:
+                if Levenshtein.distance(word, keyword.lower()) <= margin:
+                    recognizedWords.append(word)
+        return recognizedWords
+    except:
+        return []
